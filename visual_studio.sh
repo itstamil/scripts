00f2f94c280d357be3209a58a6f0a7f4e5f781b1
itstamil <36615683+itstamil@users.noreply.github.com>
2018-10-26 09:20:01 +0530

    An Script to install Visual Studio

---
+++ visual_studio.sh
@@ -0,0 +1,5 @@
+wget https://az764295.vo.msecnd.net/stable/7f3ce96ff4729c91352ae6def877e59c561f4850/code_1.28.2-1539735992_amd64.deb
+
+dpkg -i code_1.28.2-1539735992_amd64.deb
+
+apt-get install -y -f
